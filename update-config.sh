6ea4b2e16cb67b5c5e081cd99274d5765773f288
MickyC <eastwinterga@hotmail.com>
2025-10-20 23:04:19 +1100

    更新 docker-compose 設定與加入 rclone

--- update-config.sh
+++ update-config.sh
@@ -26,20 +26,28 @@ function update_bazarr_config {
     until [ -f "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml ]; do sleep 1; done
     sed -i.bak "s/base_url: ''/base_url: '\/$container'/" "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml && rm "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml.bak
     sed -i.bak "s/use_radarr: false/use_radarr: true/" "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml && rm "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml.bak
+    sed -i.bak "s/use_radarr4k: false/use_radarr4k: true/" "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml && rm "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml.bak
     sed -i.bak "s/use_sonarr: false/use_sonarr: true/" "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml && rm "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml.bak
+
     until [ -f "${CONFIG_ROOT:-.}"/sonarr/config.xml ]; do sleep 1; done
     SONARR_API_KEY=$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' "${CONFIG_ROOT:-.}"/sonarr/config.xml)
     sed -i.bak "/sonarr:/,/^radarr:/ { s/apikey: .*/apikey: $SONARR_API_KEY/; s/base_url: .*/base_url: \/sonarr/; s/ip: .*/ip: sonarr/ }" "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml && rm "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml.bak
+
     until [ -f "${CONFIG_ROOT:-.}"/radarr/config.xml ]; do sleep 1; done
     RADARR_API_KEY=$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' "${CONFIG_ROOT:-.}"/radarr/config.xml)
     sed -i.bak "/radarr:/,/^sonarr:/ { s/apikey: .*/apikey: $RADARR_API_KEY/; s/base_url: .*/base_url: \/radarr/; s/ip: .*/ip: radarr/ }" "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml && rm "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml.bak
+
+    until [ -f "${CONFIG_ROOT:-.}"/radarr4k/config.xml ]; do sleep 1; done
+    RADARR4K_API_KEY=$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' "${CONFIG_ROOT:-.}"/radarr4k/config.xml)
+    sed -i.bak "/radarr4k:/ { s/apikey: .*/apikey: $RADARR4K_API_KEY/; s/base_url: .*/base_url: \/radarr4k/; s/ip: .*/ip: radarr4k/ }" "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml && rm "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml.bak
+
     sed -i.bak 's/^BAZARR_API_KEY=.*/BAZARR_API_KEY='"$(sed -n 's/.*apikey: \(.*\)*/\1/p' "${CONFIG_ROOT:-.}"/"$container"/config/config/config.yaml | head -n 1)"'/' .env && rm .env.bak
     echo "Update of ${container} configuration complete, restarting..."
     docker compose restart "$container"
 }
 
 for container in $(docker ps --format '{{.Names}}'); do
-  if [[ "$container" =~ ^(radarr|sonarr|lidarr|prowlarr)$ ]]; then
+  if [[ "$container" =~ ^(radarr|radarr4k|sonarr|lidarr|prowlarr)$ ]]; then
     update_arr_config "$container"
   elif [[ "$container" =~ ^(bazarr)$ ]]; then
     update_bazarr_config "$container"
